86586c2e384a674d90fbd6b48de8c34c3b90c670
ehann14 <ferhan142009@gmail.com>
2026-09-04 16:38:46 +0700

    edit file routes.php

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -9,9 +9,14 @@ $routes->get('/', 'Home::index');
 
 // API Routes
 $routes->group('api', ['namespace' => 'App\Controllers\Api'], function($routes) {
+    
+    // Public route (tidak perlu login)
     $routes->post('auth/login', 'AuthController::login');
 
+    // Protected routes (wajib login / ada token JWT)
     $routes->group('', ['filter' => 'jwtAuth'], function($routes) {
+        
+        // Auth
         $routes->get('auth/me', 'AuthController::me');
         $routes->post('auth/logout', 'AuthController::logout');
 
@@ -21,5 +26,13 @@ $routes->group('api', ['namespace' => 'App\Controllers\Api'], function($routes) 
         $routes->get('vehicles/(:num)', 'VehicleController::show/$1');
         $routes->put('vehicles/(:num)', 'VehicleController::update/$1');
         $routes->delete('vehicles/(:num)', 'VehicleController::delete/$1');
+
+        // Drivers
+        $routes->get('drivers', 'DriverController::index');
+        $routes->post('drivers', 'DriverController::create');
+        $routes->get('drivers/(:num)', 'DriverController::show/$1');
+        $routes->put('drivers/(:num)', 'DriverController::update/$1');
+        $routes->delete('drivers/(:num)', 'DriverController::delete/$1');
+        
     });
 });
